a73068130f0b5e08dc824393e475fabb00944f3f
Ravi Chamarty <Ravi.Chamarty@riftio.com>
2017-01-06 11:26:04 -0500

    Update copyrights

--- 4.3/src/gen_nsd_pkg.sh
+++ 4.3/src/gen_nsd_pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright 2016 RIFT.IO Inc
+#   Copyright 2017 RIFT.IO Inc
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
--- 4.3/src/generate_descriptor_pkg.sh
+++ 4.3/src/generate_descriptor_pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############################################################################
-# Copyright 2016 RIFT.io Inc                                               #
+# Copyright 2017 RIFT.io Inc                                               #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
